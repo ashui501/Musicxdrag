f96432e955f20517f0c76861194d0ffa9b46a3ba
Drag <161552347+taslim19@users.noreply.github.com>
2025-01-05 21:56:12 +0530

    Update speedtest.py

--- AnonXMusic/plugins/tools/speedtest.py
+++ AnonXMusic/plugins/tools/speedtest.py
@@ -1,48 +1,53 @@
-# <============================================== IMPORTS =========================================================>
+import asyncio
 import speedtest
-from pyrogram import Client, filters
-from pyrogram.enums import ChatAction
+from pyrogram import filters
+from pyrogram.types import Message
 
 from AnonXMusic import app
+from AnonXMusic.misc import SUDOERS
+from AnonXMusic.utils.decorators.language import language
 
-# <=======================================================================================================>
+# Function to run the speed test
+def testspeed(m, _):
+    try:
+        test = speedtest.Speedtest()
+        test.get_best_server()
+        m = m.edit_text(_["server_12"])
+        test.download()
+        m = m.edit_text(_["server_13"])
+        test.upload()
+        test.results.share()
+        result = test.results.dict()
+        m = m.edit_text(_["server_14"])
+    except Exception as e:
+        return m.edit_text(f"<code>{e}</code>")
+    return result
 
-
-# <================================================ FUNCTION =======================================================>
-def convert(speed):
-    return round(int(speed) / 1048576, 2)
-
-
-# Handle the /speedtest command
-@app.on_message(filters.command("speedtest"))
-async def speedtestxyz(client, message):
-    # Start the speedtest
-    await app.send_chat_action(message.chat.id, ChatAction.TYPING)
-    msg = await message.reply_text("Running SpeedTest...")
-
-    # Perform SpeedTest
-    speed = speedtest.Speedtest()
-    speed.get_best_server()
-    speed.download()
-    speed.upload()
-
-    replymsg = "SpeedTest Results:"
-
-    # Send result as image or text based on user preference
-    if message.text == "/speedtest image":
-        speedtest_image = speed.results.share()
-        await message.reply_photo(photo=speedtest_image, caption=replymsg)
-    elif message.text == "/speedtest text":
-        result = speed.results.dict()
-        replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
-        await message.reply_text(replymsg, parse_mode="Markdown")
-
-    # Delete the initial "Running SpeedTest..." message
-    await msg.delete()
-
-
-# <================================================ HANDLER =======================================================>
-SPEED_TEST_HANDLER = filters.command("speedtest")
-app.add_handler(SPEED_TEST_HANDLER, speedtestxyz)
-
-# <================================================ END =======================================================>
+# Command handler for /speedtest
+@app.on_message(filters.command(["speedtest", "spt"]) & SUDOERS)
+@language
+async def speedtest_function(client, message: Message, _):
+    # Notify the user that the speed test is in progress
+    m = await message.reply_text(_["server_11"])
+    
+    # Run the speed test asynchronously
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, testspeed, m, _)
+    
+    # Format the output message with the results
+    output = _["server_15"].format(
+        result["client"]["isp"],
+        result["client"]["country"],
+        result["server"]["name"],
+        result["server"]["country"],
+        result["server"]["cc"],
+        result["server"]["sponsor"],
+        result["server"]["latency"],
+        result["ping"],
+    )
+    
+    # Send the speed test result as an image with the caption containing the details
+    msg = await message.reply_photo(photo=result["share"], caption=output)
+    
+    # Clean up and delete the waiting message
+    await m.delete()

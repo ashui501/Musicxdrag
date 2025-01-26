0e027a537b4179d8e2a386bb48cf78e0313ae4be
Drag <161552347+taslim19@users.noreply.github.com>
2025-01-26 11:56:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 200))
 
 SERVER_PLAYLIST_LIMIT = 300  # Set your desired limit here
 

feb7c8173ad0cbf4417eb3696722ccd37274a035
Drag <161552347+taslim19@users.noreply.github.com>
2025-02-12 15:13:42 +0530

    Update logger.py
    add thread feature :)

--- AnonXMusic/utils/logger.py
+++ AnonXMusic/utils/logger.py
@@ -1,25 +1,21 @@
 from pyrogram.enums import ParseMode
-
 from AnonXMusic import app
 from AnonXMusic.utils.database import is_on_off
 from config import LOGGER_ID
-
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
 <blockquote>{app.mention} ᴘʟᴀʏ ʟᴏɢ</blockquote>
-
 <blockquote><b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
 <b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
 <b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}</blockquote>
-
 <blockquote><b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
 <b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
 <b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}</blockquote>
-
 <blockquote><b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
 <b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}</blockquote>"""
+        
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
@@ -27,7 +23,8 @@ async def play_logs(message, streamtype):
                     text=logger_text,
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
+                    message_thread_id=12294  # Send to the specific thread
                 )
-            except:
-                pass
+            except Exception as e:
+                print(f"Error sending log message: {e}")  # Log the error for debugging
         return

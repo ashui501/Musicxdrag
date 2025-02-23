e5237ead0611923cdb248667dc0d4a5540103648
Drag <161552347+taslim19@users.noreply.github.com>
2025-02-23 23:10:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JIYOXC/Wleowleo",
+    "https://github.com/taslim19/musicxdrag",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
